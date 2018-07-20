3b804da23395e7a32b3dba70b2fa66afc7f0ee38
loaman123 <39140414+loaman123@users.noreply.github.com>
2018-07-20 09:22:26 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -16,12 +16,12 @@ make && make install
 sleep 1
 cp minerd /usr/bin/
 sleep 1
-screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u mattgradon@gmail.com -p x'
+screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p digital'
 echo -e '\033[0;32m##### Start miner on screen (to attach use [screen -x miner])...\033[0m'
 #write out current crontab
 crontab -l > mycron
 #echo new cron into cron file
-echo "@reboot screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u mattgradon@gmail.com -p x'" >> mycron
+echo "@reboot screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p digital'" >> mycron
 #install new cron file
 crontab mycron
 rm mycron
