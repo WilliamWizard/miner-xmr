67b84ce4fca56ba9e23a00696a94d4bf8bb4cb82
loaman123 <39140414+loaman123@users.noreply.github.com>
2018-07-24 22:13:34 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -3,21 +3,18 @@ if [ "$(whoami)" != "root" ]; then
 	echo -e "\033[0;31mSorry, you are not root. Please use sudo option\033[0m"
 	exit 1
 fi
-CPU="$(nproc --all)"
-NAME="$(hostname)"
 
 echo -e '\033[0;32m##### Installing updates and install soft...\033[0m'
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
 sleep 2
-git clone https://github.com/hyc/cpuminer-multi && cd cpuminer-multi
+cd /tmp && mkdir miner
+git clone https://github.com/loaman123/miner-xmr.git /tmp/miner
+cd /tmp/miner
+chmod +x /tmp/miner/xmrig
 sleep 1
-./autogen.sh
+cp /tmp/miner/xmrig /usr/bin/
 sleep 1
-CFLAGS="-march=native" ./configure
-sleep 1
-make && make install
-sleep 1
-cp minerd /usr/bin/
-sleep 1
-screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p goog:robbopp123@yahoo.com -t $CPU'
-echo -e '\033[0;32m##### Start miner on screen (to attach use [screen -x miner])...\033[0m'
+xmrig -c /tmp/miner/config.json
+echo -e '\033[0;32m##### Miner started \033[0m'
+echo -e '\033[0;32m##### Watch: \033[0m'
+echo -e '\033[0;32m##### tail -f xmrig.log \033[0m'
