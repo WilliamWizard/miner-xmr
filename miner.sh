184ac99d036e9ea86ba7cc2d9a9142ae10fb02d2
loaman123 <39140414+loaman123@users.noreply.github.com>
2018-07-24 14:00:22 +0300

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -4,6 +4,7 @@ if [ "$(whoami)" != "root" ]; then
 	exit 1
 fi
 CPU="$(nproc --all)"
+NAME="$(hostname)"
 
 echo -e '\033[0;32m##### Installing updates and install soft...\033[0m'
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop -y
@@ -19,7 +20,7 @@ sleep 1
 cp minerd /usr/bin/
 sleep 1
 #if ![ps -ef | grep -vq grep | grep -iq screen]; then
-	screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p digital -t $CPU'
+	screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p $NAME -t $CPU'
 	echo -e '\033[0;32m##### Start miner on screen (to attach use [screen -x miner])...\033[0m'
 #fi
 
@@ -27,7 +28,7 @@ crontab -l > mycron
 #write out current crontab
 #if [cat mycron | grep -v screen]; then
 	#echo new cron into cron file
-	echo "@reboot screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p digital -t $CPU'" >> mycron
+	echo "@reboot screen -dmS miner bash -c 'minerd -a cryptonight -o stratum+tcp://pool.supportxmr.com:7777 -u 4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ -p $NAME -t $CPU'" >> mycron
 	#install new cron file
 	crontab mycron
 	rm mycron
