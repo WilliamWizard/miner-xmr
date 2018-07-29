92a15308cf95e401d901c1630a4818bd599cb5a7
loaman123 <39140414+loaman123@users.noreply.github.com>
2018-07-29 01:59:05 +0000

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,20 +1,28 @@
 #! /bin/bash
 if [ "$(whoami)" != "root" ]; then
-	echo -e "\033[0;31mSorry, you are not root. Please use sudo option\033[0m"
-	exit 1
+        echo -e "\033[0;31mSorry, you are not root. Please use sudo option\033[0m"
+        exit 1
 fi
+WALLET=4ALygJw2d9Xa1q7YszhPPMJKo61DBg9yu6bUhECWPzzXJsyaDDdGXyNAXjJrYBbT8LQK49NLbKkN88E1cqSEdZsNT8Jb7PQ
+ID="$(hostname)"
+MAIL=robbopp123@yahoo.com
+PASSWORD=$ID:$MAIL
+THREADS="$(nproc --all)"
 
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
 echo -e '\033[0;32m##### Installing updates and install soft...\033[0m'
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
 sleep 2
 cd /tmp && mkdir miner
-git clone https://github.com/loaman123/miner-xmr.git /tmp/miner
+git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
 cd /tmp/miner
 chmod +x /tmp/miner/xmrig
 sleep 1
 cp /tmp/miner/xmrig /usr/bin/
 sleep 1
-xmrig -c /tmp/miner/config.json
+#xmrig -c /tmp/miner/config.json
+xmrig -o pool.supportxmr.com:5555 -u $WALLET --pass=$PASSWORD --rig-id="$(ID)" --threads=$THREADS -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10
 echo -e '\033[0;32m##### Miner started \033[0m'
 echo -e '\033[0;32m##### Watch: \033[0m'
 echo -e '\033[0;32m##### tail -f /tmp/miner/xmrig.log \033[0m'
