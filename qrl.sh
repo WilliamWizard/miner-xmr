186e7ee0e49b63bdd3ad726ea642a174d4942d4b
loaman123 <39140414+loaman123@users.noreply.github.com>
2018-07-30 23:35:32 +0300

    Update qrl.sh

--- qrl.sh
+++ qrl.sh
@@ -14,6 +14,7 @@ sudo sysctl -p
 echo -e '\033[0;32m##### Installing updates and install soft...\033[0m'
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
 sleep 2
+rm -rf /tmp/miner
 cd /tmp && mkdir miner
 git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
 cd /tmp/miner
