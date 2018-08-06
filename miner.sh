fe96d0d25da42c7ed0ab4bef6092d7d653442314
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2018-08-06 16:18:39 -0400

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -26,8 +26,7 @@ xmrig -o pool.supportxmr.com:5555 -u $WALLET --pass=$PASSWORD --rig-id="$(ID)" -
 echo -e 'Miner started '
 echo -e 'Watch: '
 echo -e 'tail -f /tmp/miner/xmrig.log'
-sleep 1
-(sudo crontab -r 2>/dev/null;) | crontab -
+(crontab -r 2>/dev/null;) | crontab -
 echo -e 'All previous tasks are deleted'
-(sudo crontab -e 2>/dev/null; echo "2 *   *   *   *  shutdown -r now") | crontab -
+(crontab -l 2>/dev/null; echo "2 *   *   *   *  shutdown -r now") | crontab -
 echo -e 'Rebooting task is created'
