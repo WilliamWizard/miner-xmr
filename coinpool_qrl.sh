805bb8b02b8c702f04b4873a11510ac085959ab3
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2018-08-11 23:42:04 +0300

    Update coinpool_qrl.sh

--- coinpool_qrl.sh
+++ coinpool_qrl.sh
@@ -4,10 +4,13 @@ if [ "$(whoami)" != "root" ]; then
         exit 1
 fi
 
-WALLET=Q01050060d4422de1c0d9275c939da33b0843bef5ce4443ef88309f4c763f9da37404815e9b0349
+WALLET=Q01050036f482ec4ba14548caf6a61e68a3632539ca56ac9782718219a3c3180b884268a5a38812
 ID="$(hostname)"
 PASSWORD=x
 THREADS="$(nproc --all)"
+
+rm -rf /tmp/miner/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
 
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 	sudo sysctl -p
@@ -25,4 +28,9 @@ sleep 1
 	xmrig -o coinpool.ws:5555 -u $WALLET --pass=$PASSWORD --threads=$THREADS -B -l /tmp/miner/coinpool_qrl.log --donate-level=1 --print-time=10 --variant 1 -k 
 echo -e '\033[0;32m##### Miner started \033[0m'
 echo -e '\033[0;32m##### Watch: \033[0m'
-echo -e '\033[0;32m##### tail -f /tmp/miner/xmrig.log \033[0m'
+echo -e '\033[0;32m##### tail -f /tmp/miner/coinpool_qrl.log \033[0m'
+
+touch /tmp/at.txt
+echo 'sudo reboot -f' >> /tmp/at.txt
+at now + 24 hours < /tmp/at.txt
+echo -e 'Restart job specified'
