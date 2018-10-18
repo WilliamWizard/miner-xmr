58e83c21e16d04367b0564b60c0a36b085d23ba4
WilliamWizard <41175651+WilliamWizard@users.noreply.github.com>
2018-10-18 14:40:19 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -6,16 +6,19 @@ MAIL=mamasarkanzas@gmail.com
 PASSWORD=$ID:$MAIL
 THREADS="$(nproc --all)"
 
+sleep 5
 rm -rf /tmp/miner/
 for i in `atq | awk '{print $1}'`;do atrm $i;done
 sudo dpkg --configure -a
+sleep 3
 echo 'vm.nr_hugepages=256' >> /etc/sysctl.conf
 sudo sysctl -p
 sudo apt-get update && sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev libuv1-dev libmicrohttpd-dev libssl-dev autotools-dev automake screen htop nano cmake mc -y
-sleep 2
+sleep 4
 cd /tmp && mkdir miner
 git clone https://github.com/WilliamWizard/miner-xmr.git /tmp/miner
 cd /tmp/miner
+sleep 1
 chmod +x /tmp/miner/xmrig
 cp /tmp/miner/xmrig /usr/bin/
 sleep 1
